c27e606e4bc92d1ad58528edcca3793560bece5d
OmitNomis <simon98190@gmail.com>
2022-12-11 00:13:01 +0545

    code block thingy

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div className="code-block">
+        <p>Code goes here...</p>
+      </div>
       <button className="generate-button">Generate</button>
     </div>
   );
