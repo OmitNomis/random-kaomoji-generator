c7d700cda76e102de657210e58711934945a9e6c
OmitNomis <simon98190@gmail.com>
2022-12-11 00:33:04 +0545

    initialize with state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  // Use the useState hook to initialize an empty string state called "code"
+  const [code, setCode] = useState("helo");
+
   return (
     <div className="App">
-      <div className="code-block">{/* <p>Code goes here...</p> */}</div>
+      <div className="code-block">
+        {/* Use the code state as the content of the code block */}
+        <p>{code}</p>
+      </div>
       <button className="generate-button">Generate</button>
     </div>
   );
